5013494291b9d1641dfecb00ac20b6b9483b15fc
Charbel Maroun <charbel-maroun@hotmail.com>
2023-02-17 01:55:49 -0500

    create navigation bar and import to app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+//React boostrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//components import
+import TopBar from './components/TopBar';
+
+const App = () => {
+  return (
+    <>
+    <TopBar/>
+    </>
+  );
+}
+
+export default App;
