af8299628fe62bc63633123a749021044a25bbad
Charbel Maroun <charbel-maroun@hotmail.com>
2023-02-17 03:50:33 -0500

    add Link to PlayerCard

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,28 +1,32 @@
+// React-bootstrap components import
 import Card from 'react-bootstrap/Card';
 import { Button, ButtonGroup } from 'react-bootstrap';
 
+// React-router-dom
+import { Link } from 'react-router-dom';
 
-const PlayerCard = ({ id, username, title, rating, country, url, handleShowPlayerGames}) => {
+
+const PlayerCard = ({ id, username, title, rating, country, url, handleShowPlayerGames }) => {
 
 
-  return (
-    <Card border="dark" style={{borderRadius: 0, height: "13rem"}}>
-      <Card.Body>
-        <Card.Title>{username}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Rating {rating}</Card.Subtitle>
-        <Card.Text>
-          Title: {title}
-          <br/>
-          Country: {country}
-        </Card.Text>
-        <ButtonGroup>
-        <Button xs={6}  variant='outline-dark' target="_blank" href={url}>Player Page</Button>
-        <Button xs={6} variant='outline-dark'onClick={ () => handleShowPlayerGames(username)}>Player Games</Button>
-        </ButtonGroup>
+    return (
+        <Card border="dark" style={{ borderRadius: 0, height: "13rem" }}>
+            <Card.Body>
+                <Card.Title>{username}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Rating {rating}</Card.Subtitle>
+                <Card.Text>
+                    Title: {title}
+                    <br />
+                    Country: {country}
+                </Card.Text>
+                <ButtonGroup>
+                    <Button xs={6} variant='outline-dark' target="_blank" href={url}>Player Page</Button>
+                    <Button xs={6} variant='outline-dark' as={Link} to={`/playergames/${username}`} onClick={() => handleShowPlayerGames(username)}>Player Games</Button>
+                </ButtonGroup>
 
-      </Card.Body>
-    </Card>
-  );
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default PlayerCard;
